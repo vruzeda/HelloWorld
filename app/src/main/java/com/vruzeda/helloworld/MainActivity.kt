ff2328537fc663fe20427a57d7a0d7c7e0ab998b
Vinícius Rodrigues de Uzêda <vinicius.uzeda@symphony.com>
2024-07-19 21:49:18 +0200

    Apply new project template

--- app/src/main/java/com/vruzeda/helloworld/MainActivity.kt
+++ app/src/main/java/com/vruzeda/helloworld/MainActivity.kt
@@ -1,11 +1,90 @@
 package com.vruzeda.helloworld
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import com.vruzeda.helloworld.ui.theme.HelloWorldTheme
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        enableEdgeToEdge()
+        setContent {
+            MainActivityContent()
+        }
+    }
+}
+
+@Composable
+fun MainActivityContent() {
+    HelloWorldTheme {
+        Scaffold(
+            topBar = {
+                HelloWorldAppBar()
+            },
+            modifier = Modifier.fillMaxSize()
+        ) { innerPadding ->
+            Column(
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Center,
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(innerPadding)
+            ) {
+                Greeting()
+            }
+        }
+    }
+}
+
+@Composable
+@OptIn(ExperimentalMaterial3Api::class)
+fun HelloWorldAppBar(modifier: Modifier = Modifier) {
+    TopAppBar(
+        title = { Text(stringResource(R.string.app_name)) },
+        colors = TopAppBarDefaults.mediumTopAppBarColors(
+            containerColor = MaterialTheme.colorScheme.primaryContainer
+        ),
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun HelloWorldAppBarPreview() {
+    HelloWorldTheme {
+        HelloWorldAppBar()
+    }
+}
+
+@Composable
+fun Greeting(modifier: Modifier = Modifier) {
+    Text(
+        text = stringResource(R.string.app_name),
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    HelloWorldTheme {
+        Greeting()
     }
 }
---
+++ playStore/privacyPolicy.md
@@ -0,0 +1,15 @@
+# Privacy Policy
+
+This privacy policy applies to the Hello World! app (hereby referred to as "Application") for mobile
+devices that was created by Vinícius Rodrigues de Uzêda (hereby referred to as "Service Provider")
+as a Free service. This service is intended for use "AS IS".
+
+## Information Collection and Use
+
+The Application collects no information when you download and use it.
+The Application does not gather precise information about the location of your mobile device.
+
+## Contact Us
+
+If you have any questions regarding privacy while using the Application, or have questions about the
+practices, please contact the Service Provider via email at vruzeda+helloworld@gmail.com.
